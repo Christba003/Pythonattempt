caf4f1418e5f7027e56ee3850fef858a4a4f6d8a
DESKTOP-RGPMT3T\Christian <christba455@gmail.com>
2021-12-20 17:53:24 -0600

    Third try

---
+++ Tercer_intento.py
@@ -0,0 +1,10 @@
+#
+
+class Vehicle:
+    def __init__(self,name,max_speed,miliage):
+        self.made = name
+        self.max_speed = max_speed
+        self.miliage = miliage
+civic = Vehicle ("Rojo",200,12)
+print(civic)
+
